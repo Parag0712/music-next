33da3d2492e961224a7b4201ae816459bf59e91e
Parag0712 <paragvadgama123@gmail.com>
2024-02-22 00:05:56 +0530

    Hero Section Made

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,10 @@
+import HeroSection from '@/components/HeroSection'
 import React from 'react'
 
 function page() {
   return (
     <main className='min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.02]'>
-      <div>Next First Project</div>
+      <HeroSection />
     </main>
   )
 }
