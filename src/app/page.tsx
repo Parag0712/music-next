4d3892ddcb586dbeafb45a65bc47bf12b2a70bac
Parag0712 <paragvadgama123@gmail.com>
2024-02-22 23:19:54 +0530

    Card Section Done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,15 @@
+import { Card } from '@/components/Card'
 import HeroSection from '@/components/HeroSection'
 import React from 'react'
 
+
 function page() {
+
   return (
-    <main className='min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.02]'>
+    
+    <main className='min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.02] '>
       <HeroSection />
+      <Card/>            
     </main>
   )
 }
